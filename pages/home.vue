a38b797668943dccdb3e24228b60a6063c4dc634
johnsqliu <johnsqliu@tencent.com>
2021-08-31 00:12:03 +0800

    init project

---
+++ pages/home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home-page">
+    <h3>home</h3>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="index">
+  <NuxtLink to="/home">home page</NuxtLink>
+  <Tutorial/>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
